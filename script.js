18e7c46b5dc43edc3a7547b53f2750b7cd0c1c6d
Martina Scibilia <martina.scibilia@outlook.com>
2025-05-09 16:55:45 +0200

    Add functionality for select

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-function updateTime() {
+function displayTime() {
   let romeElement = document.querySelector("#rome");
-  let romeDateElement = romeElement.querySelector(".date");
-  let romeTimeElement = romeElement.querySelector(".time");
-  let rome = moment().tz("Europe/Rome");
+  if (romeElement) {
+    let romeDateElement = romeElement.querySelector(".date");
+    let romeTimeElement = romeElement.querySelector(".time");
+    let rome = moment().tz("Europe/Rome");
 
-  romeDateElement.innerHTML = rome.format("MMMM Do YYYY");
-  romeTimeElement.innerHTML = rome.format("h:mm:ss [<small>]A[</small>]");
-
-  let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYork = moment().tz("America/New_York");
-
-  newYorkDateElement.innerHTML = newYork.format("MMMM Do YYYY");
-  newYorkTimeElement.innerHTML = newYork.format("h:mm:ss [<small>]A[</small>]");
+    romeDateElement.innerHTML = rome.format("MMMM Do YYYY");
+    romeTimeElement.innerHTML = rome.format("h:mm:ss [<small>]A[</small>]");
+  }
 }
 
-updateTime();
-setInterval(updateTime, 1000);
+displayTime();
+setInterval(displayTime, 1000);
+
+function changeCity(event) {
+  let timeZone = event.target.value;
+  let city = timeZone.replace("_", " ").split("/")[1];
+  let time = moment().tz(timeZone).format("h:mm:ss [<small>]A[</small>]");
+  let date = moment().tz(timeZone).format("MMMM Do YYYY");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${city}</h2>
+            <div class="date">${date}</div>
+          </div>
+          <div class="time">${time}</div>
+        </div>`;
+}
+
+let citySelect = document.querySelector("#city-select");
+citySelect.addEventListener("change", changeCity);
